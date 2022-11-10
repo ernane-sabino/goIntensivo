c419131b63b9760d9c5459cf96c31d86f83b42a0
Ernane Sabino <esabino@stefanini.com>
2022-11-10 09:15:00 -0300

    GO :: HTTP

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/ernane-sabino/goIntensivo/internal/order/infra/database"
@@ -32,7 +33,26 @@ func main() {
 	out := make(chan amqp.Delivery) // channel
 	go rabbitmq.Consume(ch, out)    // T2
 
-	for msg := range out {
+	qtdWorkers := 150
+	for i := 1; i <= qtdWorkers; i++ {
+		go worker(out, &uc, i) // Criando mais threads
+	}
+
+	http.HandleFunc("/total", func(w http.ResponseWriter, r *http.Request) {
+		getTotalUC := usecase.GetTotalUseCase{OrderRepository: repository}
+		total, err := getTotalUC.Execute()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+		}
+		json.NewEncoder(w).Encode(total)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func worker(deliveryMessage <-chan amqp.Delivery, uc *usecase.CalculateFinalPriceUseCase, workerID int) {
+	for msg := range deliveryMessage {
 		var inputDTO usecase.OrderInputDTO
 		err := json.Unmarshal(msg.Body, &inputDTO)
 		if err != nil {
@@ -43,7 +63,7 @@ func main() {
 			panic(err)
 		}
 		msg.Ack(false)
-		fmt.Println(outputDTO)
-		time.Sleep(500 * time.Millisecond)
+		fmt.Printf("Worker %d has processed order %s\n", workerID, outputDTO.ID)
+		time.Sleep(1 * time.Second)
 	}
 }
